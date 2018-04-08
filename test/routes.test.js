9729a7379a0aff67649fcaba12085189f6c4f505
FabienGreard <grd.fabien@gmail.com>
2018-04-08 17:16:45 +0200

    routes url

--- test/routes.test.js
+++ test/routes.test.js
@@ -2,15 +2,24 @@ const getDirectories = require('../utils/getDirectories'),
   request = require('supertest'),
   app = require('../server.js');
 
-test('renders routes url', done => {
-  const directories = getDirectories('routes');
-  for (directory of directories) {
-    request(app)
-      .get(`${directory.url}`)
-      .expect(301)
-      .end(err => {
-        if (err) throw done(err);
-        done();
-      });
-  }
+describe('Routes', () => {
+  test('Should render routes url', done => {
+    const directories = getDirectories('routes');
+    for (directory of directories) {
+      request(app)
+        .get(`${directory.url}`)
+        .expect(301)
+        .end(err => {
+          if (err) throw done(err);
+          done();
+        });
+    }
+  });
+
+  test('Should not match', () => {
+    const directories = getDirectories('this-should-never-match')[
+      Symbol.iterator
+    ]();
+    expect(directories.next()).toEqual({ done: true });
+  });
 });
