1893ce4145a743b100d9946cb60ac830f8aeb8ca
FabienGreard <grd.fabien@gmail.com>
2018-06-08 09:08:16 +0200

    seo + errorHandler scope

---
+++ utils/index.js
@@ -0,0 +1,6 @@
+const getDirectories = require('./getDirectories'),
+  seo = require('./seo'),
+  errorHandler = require('./errorHandler'),
+  basicAuth = require('./basicAuth');
+
+module.exports = { getDirectories, seo, errorHandler, basicAuth };
