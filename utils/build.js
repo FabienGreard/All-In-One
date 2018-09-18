57f3608270cf43012f5244ece418247f8a369fa8
FabienGreard <grd.fabien@gmail.com>
2018-09-18 16:07:48 +0200

    clean build

--- utils/build.js
+++ utils/build.js
@@ -3,25 +3,16 @@ const uglifyJS = require('uglify-js'),
   autoprefixer = require('autoprefixer'),
   fs = require('fs');
 
-const writeFile = (path, data) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(path, data, err => {
-      if (err) reject(err);
-      else resolve(path);
-    });
-  });
-};
-
-const js = async path => {
+const js = path => {
   const data = fs.readFileSync(path, 'utf-8');
   const { code } = uglifyJS.minify(data);
-  await writeFile(path, code);
+  fs.writeFileSync(path, code);
 };
 
-const css = async path => {
+const css = path => {
   const data = fs.readFileSync(path, 'utf-8');
   const { styles } = new cleanCss({ compatibility: '*' }).minify(data);
-  await writeFile(path, styles);
+  fs.writeFileSync(path, styles);
 };
 
 module.exports = {
